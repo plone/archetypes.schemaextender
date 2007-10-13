42ab39058124fc23e17223c9e977b492a7e93dfa
Martin Aspeli <optilude@gmail.com>
2007-10-13 15:02:53 +0000

    Wording and whitespace
    svn path=/archetypes.schemaextender/trunk/; revision=8708

--- archetypes/schemaextender/interfaces.py
+++ archetypes/schemaextender/interfaces.py
@@ -1,19 +1,20 @@
 from zope.interface import Interface
 
-
 class ISchemaExtender(Interface):
     """Interface for adapters that extend the schema.
     """
     
     def getFields():
-        """Returns fields to be added to the schema."""
-
+        """Returns a list of fields to be added to the schema.
+        """
 
 class IOrderableSchemaExtender(ISchemaExtender):
     def getOrder(original):
         """Returns the optionally reordered fields.
 
-           Takes a dictionary where the keys are the schema name and the
-           values are the names of the fields in original order, returns an
-           optionally reordered dictionary.
+        'original' is a dictionary where the keys are the names of
+        schemata and the keys are lists of field names, in order.
+        
+        The method should return a new such dictionary with re-ordered
+        lists.
         """
