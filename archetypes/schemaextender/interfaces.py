fa967e6598e265e9162032bd5e36a496aff86bb7
Erik Rose <psucorp@grinchcentral.com>
2007-11-08 20:58:18 +0000

    Updated a few docstrings for accuracy and PEP 8-compliance.
    svn path=/archetypes.schemaextender/trunk/; revision=8886

--- archetypes/schemaextender/interfaces.py
+++ archetypes/schemaextender/interfaces.py
@@ -1,22 +1,19 @@
 from zope.interface import Interface
 
 class ISchemaExtender(Interface):
-    """Interface for adapters that extend the schema.
-    """
+    """Interface for adapters that extend the schema"""
     
     def getFields():
-        """Returns a list of fields to be added to the schema.
-        """
+        """Return a list of fields to be added to the schema."""
 
 class IOrderableSchemaExtender(ISchemaExtender):
-    """An orderable version of the schema extender
-    """
+    """An orderable version of the schema extender"""
     
     def getOrder(original):
-        """Returns the optionally reordered fields.
+        """Return the optionally reordered fields.
 
         'original' is a dictionary where the keys are the names of
-        schemata and the keys are lists of field names, in order.
+        schemata and the values are lists of field names, in order.
         
         The method should return a new such dictionary with re-ordered
         lists.
