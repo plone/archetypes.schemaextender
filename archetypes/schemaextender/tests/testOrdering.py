2d4438af4cb6bea9d70f4c427ab91a3ee68ad2ec
Wichert Akkerman <wichert@wiggy.net>
2007-12-04 15:47:57 +0000

    Test schema reordering
    svn path=/archetypes.schemaextender/branches/wichert-test-simplification/; revision=9050

--- archetypes/schemaextender/tests/testOrdering.py
+++ archetypes/schemaextender/tests/testOrdering.py
@@ -1,7 +1,9 @@
 import unittest
 from Products.Archetypes.public import Schema
 from Products.Archetypes.public import ManagedSchema
+from Products.Archetypes.utils import OrderedDict
 from archetypes.schemaextender.extender import get_schema_order
+from archetypes.schemaextender.extender import set_schema_order
 from archetypes.schemaextender.tests.mocks import MockField
 
 class GetSchemaOrderTests(unittest.TestCase):
@@ -34,7 +36,65 @@ class GetSchemaOrderTests(unittest.TestCase):
         self.assertEqual(order, {"default": ["two", "one"]})
 
 
+class SetSchemaOrderTests(unittest.TestCase):
+    def testEmptySchema(self):
+        schema=Schema()
+        before=schema.signature()
+        set_schema_order(schema, {})
+        self.assertEqual(schema.signature(), before)
+
+    def testNopReorderErrors(self):
+        schema=Schema()
+        schema.addField(MockField("one"))
+        schema.addField(MockField("two"))
+        self.assertRaises(ValueError, set_schema_order, schema, {})
+
+    def testIdentityFieldReorder(self):
+        schema=Schema()
+        schema.addField(MockField("one"))
+        schema.addField(MockField("two"))
+        set_schema_order(schema, {"default" : ["one", "two"]})
+        self.assertEqual(schema._names, ["one", "two"])
+
+    def testSwapTwoFields(self):
+        schema=Schema()
+        schema.addField(MockField("one"))
+        schema.addField(MockField("two"))
+        set_schema_order(schema, {"default" : ["two", "one"]})
+        self.assertEqual(schema._names, ["two", "one"])
+
+    def testIdentitySchemataReorder(self):
+        schema=ManagedSchema()
+        schema.addField(MockField("one", "one"))
+        schema.addField(MockField("two", "two"))
+        command=OrderedDict()
+        command["one"]=["one"]
+        command["two"]=["two"]
+        set_schema_order(schema, command)
+        self.assertEqual(schema.getSchemataNames(), ["one", "two"])
+
+    def testSwapTwoSchemata(self):
+        schema=ManagedSchema()
+        schema.addField(MockField("one", "one"))
+        schema.addField(MockField("two", "two"))
+        command=OrderedDict()
+        command["two"]=["two"]
+        command["one"]=["one"]
+        set_schema_order(schema, command)
+        self.assertEqual(schema.getSchemataNames(), ["two", "one"])
+
+    def testMoveFieldToOtherSchemata(self):
+        schema=ManagedSchema()
+        schema.addField(MockField("one", "one"))
+        schema.addField(MockField("two", "two"))
+        set_schema_order(schema, {"one" : [ "one", "two" ]})
+        self.assertEqual(schema.getSchemataNames(), ["one"])
+        self.assertEqual(schema._names, ["one", "two"])
+
+
 def test_suite():
     suite=unittest.TestSuite()
     suite.addTest(unittest.makeSuite(GetSchemaOrderTests))
+    suite.addTest(unittest.makeSuite(SetSchemaOrderTests))
     return suite
+
