68e1cc198aed0f5305a13515d457ffef14621497
Wichert Akkerman <wichert@wiggy.net>
2007-12-04 12:52:54 +0000

    More complete tests
    svn path=/archetypes.schemaextender/trunk/; revision=9028

--- archetypes/schemaextender/benchmark.py
+++ archetypes/schemaextender/benchmark.py
@@ -1,27 +1,44 @@
 import time
 from zope.interface import implements
+from zope.interface import implementsOnly
 from zope.interface import classImplements
 from zope.component import adapts
 from zope.component import provideAdapter
+from zope.component import getGlobalSiteManager
 from archetypes.schemaextender.extender import instanceSchemaFactory
 from archetypes.schemaextender.interfaces import IExtensible
+from archetypes.schemaextender.interfaces import ISchemaExtender
 from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
+from archetypes.schemaextender.interfaces import ISchemaModifier
 from Products.Archetypes.public import BaseObject
 
 class SimpleType(BaseObject):
     """A very simple AT type."""
 
 
-class DummyExtender(object):
-    implements(IOrderableSchemaExtender)
+class Extender(object):
+    implements(ISchemaExtender)
     adapts(SimpleType)
 
     def __init__(self, context):
         pass
     def getFields(self):
         return []
+
+class OrderableExtender(Extender):
+    implementsOnly(IOrderableSchemaExtender)
+    adapts(SimpleType)
+
     def getOrder(self, original):
         return original
+
+class SchemaModifier(object):
+    implements(ISchemaModifier)
+    adapts(SimpleType)
+    def __init__(self, context):
+        pass
+    def fiddle(self, schema):
+        pass
 
 def bench():
     i=5000
@@ -38,7 +55,18 @@ print "Benchmark without atse: %.2f seconds" % bench()
 
 classImplements(SimpleType, IExtensible)
 provideAdapter(instanceSchemaFactory)
-provideAdapter(DummyExtender, name=u"atse.benchmark")
+sm=getGlobalSiteManager()
 
-print "Benchmark with atse: %.2f seconds" % bench()
+provideAdapter(Extender, name=u"atse.benchmark")
+print "Benchmark with Extender: %.2f seconds" % bench()
+sm.unregisterAdapter(Extender, name=u"atse.benchmark")
+
+
+provideAdapter(OrderableExtender, name=u"atse.benchmark")
+print "Benchmark with OrderableExtender: %.2f seconds" % bench()
+sm.unregisterAdapter(OrderableExtender, name=u"atse.benchmark")
+
+provideAdapter(SchemaModifier, name=u"atse.benchmark")
+print "Benchmark with SchemaModifier: %.2f seconds" % bench()
+sm.unregisterAdapter(SchemaModifier, name=u"atse.benchmark")
 
