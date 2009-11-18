f126713c9fcd8ac17395a847b286fb4c55d940fc
Hanno Schlichting <hanno@hannosch.eu>
2009-11-18 13:35:46 +0000

    Prepare release
    svn path=/archetypes.schemaextender/trunk/; revision=11718

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(name='archetypes.schemaextender',
       version=version,
       description="Dynamically extend Archetypes schemas with named adapters.",
       long_description=open("README.txt").read() + \
-                        open("docs/INSTALL.txt").read() + \
-                        open("docs/HISTORY.txt").read(),
+                        open("CHANGES.txt").read(),
       classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
