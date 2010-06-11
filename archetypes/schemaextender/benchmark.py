79097079c4e5d9dcbe0d85c019b81d71e190f89d
Hanno Schlichting <hanno@hannosch.eu>
2010-06-11 17:48:16 +0000

    Extend benchmark to cover the cached case and test the actual Schema() call
    svn path=/archetypes.schemaextender/trunk/; revision=12613

--- archetypes/schemaextender/benchmark.py
+++ archetypes/schemaextender/benchmark.py
@@ -11,6 +11,7 @@ import time
 from zope.interface import classImplements
 from zope.component import provideAdapter
 from zope.component import getGlobalSiteManager
+from archetypes.schemaextender.extender import cachingInstanceSchemaFactory
 from archetypes.schemaextender.extender import instanceSchemaFactory
 from archetypes.schemaextender.interfaces import IExtensible
 from archetypes.schemaextender.tests.mocks import ExtensibleType
@@ -19,14 +20,17 @@ from archetypes.schemaextender.tests.mocks import OrderableExtender
 from archetypes.schemaextender.tests.mocks import SchemaModifier
 
 
+class MockRequest(object):
+    pass
+
+
 def bench():
-    i=5000
-    a=ExtensibleType("id")
-    start=time.time()
-    while i:
-        a._updateSchema()
-        i-=1
-    delta=time.time()-start
+    a = ExtensibleType("id")
+    a.REQUEST = MockRequest()
+    start = time.time()
+    for i in range(10000):
+        a.Schema()
+    delta = time.time() - start
     return delta
 
 print "Starting benchmark"
@@ -40,11 +44,25 @@ provideAdapter(Extender, name=u"atse.benchmark")
 print "Benchmark with Extender: %.2f seconds" % bench()
 sm.unregisterAdapter(Extender, name=u"atse.benchmark")
 
-
 provideAdapter(OrderableExtender, name=u"atse.benchmark")
 print "Benchmark with OrderableExtender: %.2f seconds" % bench()
 sm.unregisterAdapter(OrderableExtender, name=u"atse.benchmark")
 
 provideAdapter(SchemaModifier, name=u"atse.benchmark")
 print "Benchmark with SchemaModifier: %.2f seconds" % bench()
+sm.unregisterAdapter(SchemaModifier, name=u"atse.benchmark")
+
+sm.unregisterAdapter(instanceSchemaFactory)
+provideAdapter(cachingInstanceSchemaFactory)
+
+provideAdapter(Extender, name=u"atse.benchmark")
+print "Benchmark with cached Extender: %.2f seconds" % bench()
+sm.unregisterAdapter(Extender, name=u"atse.benchmark")
+
+provideAdapter(OrderableExtender, name=u"atse.benchmark")
+print "Benchmark with cached OrderableExtender: %.2f seconds" % bench()
+sm.unregisterAdapter(OrderableExtender, name=u"atse.benchmark")
+
+provideAdapter(SchemaModifier, name=u"atse.benchmark")
+print "Benchmark with cached SchemaModifier: %.2f seconds" % bench()
 sm.unregisterAdapter(SchemaModifier, name=u"atse.benchmark")
