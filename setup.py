5e6f04bbb5917071c27ef728f0c53a798cfad8e4
Maurits van Rees <maurits@vanrees.org>
2018-10-01 17:52:06 +0200

    Removed unneeded exclude ez_setup.py from setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email='plone-developers@lists.sourceforge.net',
     url='https://pypi.org/project/archetypes.schemaextender',
     license='GPL',
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages(),
     namespace_packages=['archetypes'],
     include_package_data=True,
     zip_safe=False,
