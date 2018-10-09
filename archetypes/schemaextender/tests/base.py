b7453eb9ffaf7c0193be2f47fba9b1a18272fb77
Peter Mathis <peter.mathis@kombinat.at>
2018-10-09 08:37:00 +0200

    remove layer and manually push/pop global registry

--- archetypes/schemaextender/tests/base.py
+++ archetypes/schemaextender/tests/base.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from archetypes.schemaextender.extender import instanceSchemaFactory
 from archetypes.schemaextender.interfaces import IExtensible
+from plone.app.testing import helpers
 from plone.app.testing.bbb_at import PloneTestCase
 from plone.testing import zca
 from Products.Archetypes.public import BaseObject
@@ -11,8 +12,16 @@ import unittest
 
 
 class ASTestCase(PloneTestCase):
-    """ Base class for testing archetypes.schemaextender """
-    layer = zca.UNIT_TESTING
+    """ Base class for testing archetypes.schemaextender
+        we manually push and pop global registry here to avoid
+        leaking extended fields into other testcases
+    """
+
+    def beforeSetUp(self):
+        helpers.pushGlobalRegistry(self.layer['portal'])
+
+    def afterTearDown(self):
+        helpers.popGlobalRegistry(self.layer['portal'])
 
 
 @implementer(IExtensible)
