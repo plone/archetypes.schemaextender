3747f2edaf9f9cf1227cac33dae8dfc728dfbd95
Maurits van Rees <maurits@vanrees.org>
2021-10-07 15:56:04 +0200

    Added 'Python 2 only' to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
     ],
     keywords='Archetypes Schema extend',
     author='Florian Schulze',
